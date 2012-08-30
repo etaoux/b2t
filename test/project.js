b42756145489344ffce79267d3520c319e481f30
goto100 <goto100@gmail.com>
2012-08-30 17:38:41 +0800

    make component-preview run

--- test/project.js
+++ test/project.js
@@ -1,14 +1,36 @@
 var assert = require('assert');
-var pjct = require('../lib/b2t/project');
+var Project = require('../lib/b2t/Project');
 var path = require('path');
 
 var projectRoot = path.join(__dirname, '../demo/package');
 
 describe('project base', function() {
 	it('members', function(done) {
-		var project = pjct.load(projectRoot);
+		var project = Project.load(projectRoot);
+		assert.equal(project.root, projectRoot);
 		assert.equal(project.name, 'market1');
 		assert.equal(project.version, '0.0.1');
 		done();
+	});
+});
+
+describe('project page', function() {
+	var project = Project.load(projectRoot);
+	it('base', function(done) {
+		project.getPages(function(err, pages) {
+			assert.equal(pages.length, 1);
+			done();
+		});
+	});
+	it('template', function(done) {
+		// jsdom not installed
+		done();
+		return;
+		project.getPage('home', function(err, page) {
+			page.getTemplate(function(err, template) {
+				console.log(template);
+				done();
+			})
+		});
 	});
 });

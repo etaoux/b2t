dc94d3c19d58252dc1b1df43ae468db9680d949a
goto100 <goto100@gmail.com>
2012-09-13 17:41:10 +0800

    make sync. and unit test pass'

--- test/component.js
+++ test/component.js
@@ -6,43 +6,40 @@ var componentRoot = path.join(__dirname, '../demo/project/components/header');
 
 describe('component base', function() {
 	it('members', function(done) {
-		Component.load(componentRoot, function(err, component) {
-			assert.equal(component.root, componentRoot);
-			assert.equal(component.name, 'b2t_demo_header');
-			assert.equal(component.version, '0.0.1');
-			done();
-		});
+		var component = Component.load(componentRoot);
+		assert.equal(component.root, componentRoot);
+		assert.equal(component.name, 'b2t_demo_header');
+		assert.equal(component.version, '0.0.1');
+		done();
 	});
 });
 
 describe('component skin', function() {
-	Component.load(componentRoot, function(err, component) {
-		it('skin', function(done) {
-			component.getSkin('default', function(err, skin) {
-				assert.equal(skin.sizes.length, 3);
-				done();
-			});
+	var component = Component.load(componentRoot);
+	it('skin', function(done) {
+		component.getSkin('default', function(err, skin) {
+			assert.equal(skin.sizes.length, 3);
+			done();
 		});
-		it('toCSS', function(done) {
-			component.getSkin('default', function(err, skin) {
-				skin.toCSS(function(err, css) {
-					done();
-				});
+	});
+	it('toCSS', function(done) {
+		component.getSkin('default', function(err, skin) {
+			skin.toCSS(function(err, css) {
+				done();
 			});
 		});
 	});
 });
 
 describe('component template', function() {
-	Component.load(componentRoot, function(err, component) {
-		it('template', function(done) {
-			assert.equal(!!component.template, true);
+	var component = Component.load(componentRoot);
+	it('template', function(done) {
+		assert.equal(!!component.template, true);
+		done();
+	});
+	it('toHTML', function(done) {
+		component.toHTML({title: 'title'}, function(err, result) {
 			done();
-		});
-		it('toHTML', function(done) {
-			component.toHTML({title: 'title'}, function(err, result) {
-				done();
-			});
 		});
 	});
 });

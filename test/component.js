25ba0e005b433cb6b058f508c267e984859603aa
goto100 <goto100@gmail.com>
2012-09-18 10:57:00 +0800

    demo files

--- test/component.js
+++ test/component.js
@@ -6,40 +6,43 @@ var componentRoot = path.join(__dirname, '../demo/project/components/header');
 
 describe('component base', function() {
 	it('members', function(done) {
-		var component = Component.load(componentRoot);
-		assert.equal(component.root, componentRoot);
-		assert.equal(component.name, 'b2t_demo_header');
-		assert.equal(component.version, '0.0.1');
-		done();
+		Component.load(componentRoot, function(err, component) {
+			assert.equal(component.root, componentRoot);
+			assert.equal(component.name, 'b2t_demo_header');
+			assert.equal(component.version, '0.0.1');
+			done();
+		});
 	});
 });
 
 describe('component skin', function() {
-	var component = Component.load(componentRoot);
-	it('skin', function(done) {
-		component.getSkin('default', function(err, skin) {
-			assert.equal(skin.sizes.length, 3);
-			done();
-		});
-	});
-	it('toCSS', function(done) {
-		component.getSkin('default', function(err, skin) {
-			skin.toCSS(function(err, css) {
+	Component.load(componentRoot, function(err, component) {
+		it('skin', function(done) {
+			component.getSkin('default', function(err, skin) {
+				assert.equal(skin.sizes.length, 3);
 				done();
+			});
+		});
+		it('toCSS', function(done) {
+			component.getSkin('default', function(err, skin) {
+				skin.toCSS(function(err, css) {
+					done();
+				});
 			});
 		});
 	});
 });
 
 describe('component template', function() {
-	var component = Component.load(componentRoot);
-	it('template', function(done) {
-		assert.equal(!!component.template, true);
-		done();
-	});
-	it('toHTML', function(done) {
-		component.toHTML({title: 'title'}, function(err, result) {
+	Component.load(componentRoot, function(err, component) {
+		it('template', function(done) {
+			assert.equal(!!component.template, true);
 			done();
+		});
+		it('toHTML', function(done) {
+			component.toHTML({title: 'title'}, function(err, result) {
+				done();
+			});
 		});
 	});
 });
--- test/project.js
+++ test/project.js
@@ -6,11 +6,12 @@ var projectRoot = path.join(__dirname, '../demo/project');
 
 describe('project base', function() {
 	it('members', function(done) {
-		var project = Project.load(projectRoot);
-		assert.equal(project.root, projectRoot);
-		assert.equal(project.name, 'market1');
-		assert.equal(project.version, '0.0.1');
-		done();
+		Project.load(projectRoot, function(err, project) {
+			assert.equal(project.root, projectRoot);
+			assert.equal(project.name, 'market1');
+			assert.equal(project.version, '0.0.1');
+			done();
+		});
 	});
 });
 
@@ -20,25 +21,26 @@ describe('project components', function() {
 });
 
 describe('project page', function() {
-	var project = Project.load(projectRoot);
-	it('base', function(done) {
-		project.getPages(function(err, pages) {
-			assert.equal(pages.length, 1);
-			done();
-		});
-	});
-	it('template', function(done) {
-		project.getPage('home', function(err, page) {
-			page.getTemplate(function(err, template) {
-				assert.ok(template);
+	Project.load(projectRoot, function(err, project) {
+		it('base', function(done) {
+			project.getPages(function(err, pages) {
+				assert.equal(pages.length, 1);
 				done();
-			})
+			});
 		});
-	});
-	it('build', function(done) {
-		project.getPage('home', function(err, page) {
-			// TODO
-			done();
+		it('template', function(done) {
+			project.getPage('home', function(err, page) {
+				page.getTemplate(function(err, template) {
+					assert.ok(template);
+					done();
+				})
+			});
+		});
+		it('build', function(done) {
+			project.getPage('home', function(err, page) {
+				// TODO
+				done();
+			});
 		});
 	});
 });
